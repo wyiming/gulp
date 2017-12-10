1a3088339f54e3d2ad8232bdeb2e17a997dad180
wyiming <m15225423223@163.com>
2017-12-10 20:47:45 +0800

    更新任务依赖

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,8 @@ var rev = require('gulp-rev'); //- 对文件名加MD5后缀
 var revCollector = require('gulp-rev-collector'); //- 路径替换
 var del = require('del');
 var imagemin = require('gulp-imagemin'); //图片压缩
-gulp.task('static', function() { //- 创建一个名为 concat 的 task   gulp.src(['./src/css/*.css'])
-    gulp.src(['webapp/**/*.html', 'webapp/*.html']) //  .pipe(concat('wrap.min.css'))              //- 合并后的文件名
-        .pipe(gulp.dest('maven/'));
-});
 gulp.task('cssmin', function() { //- 创建一个名为 concat 的 task   gulp.src(['./src/css/*.css'])
-    gulp.src(['webapp/**/*.css']) //  .pipe(concat('wrap.min.css'))              //- 合并后的文件名
+    return gulp.src(['webapp/**/*.css']) //  .pipe(concat('wrap.min.css'))              //- 合并后的文件名
         .pipe(minifyCss()) //- 压缩处理成一行
         .pipe(rev()) //- 文件名加MD5后缀
         .pipe(gulp.dest('maven/')) //- 输出文件本地
@@ -19,15 +15,16 @@ gulp.task('cssmin', function() { //- 创建一个名为 concat 的 task   gulp.s
         .pipe(gulp.dest('rev/revcss/')) //- 将 rev-manifest.json 保存到 rev 目录内
 });
 gulp.task('jsmin', function() {
-    gulp.src(['webapp/**/*.js'])
+    return gulp.src(['webapp/**/*.js'])
         .pipe(uglify())
         .pipe(rev())
         .pipe(gulp.dest('maven/'))
         .pipe(rev.manifest())
         .pipe(gulp.dest('rev/revjs/'))
 })
-gulp.task('rev', function() {
-    gulp.src(['rev/*/*.json', 'webapp/**/*.html', 'webapp/*.html'])
+gulp.task('rev', ['cssmin', 'jsmin', 'images'], function() {
+    //此处数字内的任务要依赖rev这个任务需先数组的内容在执行rev保证第一次就能替换html的URL  必须在gulp.src之前加return才行
+    return gulp.src(['rev/*/*.json', 'webapp/**/*.html', 'webapp/*.html'])
         //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
         .pipe(revCollector())
         //- 执行文件内css名的替换
@@ -35,13 +32,14 @@ gulp.task('rev', function() {
     //- 替换后的文件输出的目录
 });
 
+
 gulp.task('images', function() {
-    gulp.src(['webapp/**/*.{png,jpg,gif,ico}'])
+    return gulp.src(['webapp/**/*.{png,jpg,gif,ico}'])
         .pipe(imagemin())
         .pipe(gulp.dest('maven/'));
     //- 替换后的文件输出的目录
 });
 gulp.task('clean', function(cb) {
-    del(['maven/'], cb)
+    del(['maven/', 'rev'], cb)
 })
-gulp.task('default', ['cssmin', 'jsmin', 'images', 'rev'])
+gulp.task('default', ['rev'])
